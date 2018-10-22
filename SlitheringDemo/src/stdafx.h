f000c2573d9506a0fbdd124f1b060c05efbc6684
Rafael Plugge <c00203250@itcarlow.ie>
2018-10-22 18:45:43 +0100

    Created initial solution and demo project.
    Set following project settings:
    - compile with cpp17
    - proper linkage to library directories in both release and debug modes
    on both a solution level and project level.
    - set post-build event to copy over dlls in binary folder at both a
    solution level and project level.

---
+++ SlitheringDemo/src/stdafx.h
@@ -0,0 +1,84 @@
+﻿// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#ifdef _DEBUG
+
+/*********************************/
+/**    BEGIN Debug Libraries    **/
+/*********************************/
+/*********************************/
+/**     END Debug Libraries     **/
+/*********************************/
+
+#else
+
+/*********************************/
+/**   BEGIN Release Libraries   **/
+/*********************************/
+/*********************************/
+/**    END Release Libraries    **/
+/*********************************/
+
+#endif
+
+/*********************************/
+/** BEGIN Independant Libraries **/
+/*********************************/
+/*********************************/
+/**  END Independant Libraries  **/
+/*********************************/
+
+// STL Common C libraries
+#include <cstddef>
+#include <cstdint>
+
+// STL Containers
+#include <iterator>
+#include <array>
+#include <list>
+#include <forward_list>
+#include <set>
+#include <map>
+#include <vector>
+#include <stack>
+#include <queue>
+
+// STL Utility libraries
+#include <exception>
+#include <type_traits>
+#include <memory>
+#include <random>
+#include <chrono>
+#include <functional>
+#include <tuple>
+#include <initializer_list>
+#include <limits>
+#include <variant>
+#include <optional>
+
+// STL Input/Output libraries
+#include <string>
+#include <sstream>
+#include <istream>
+#include <ostream>
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+
+// STL Multithreading libraries
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <shared_mutex>
+#include <future>
+#include <condition_variable>
+
+/*********************************/
+/**         Includes here       **/
+/*********************************/
+
+
